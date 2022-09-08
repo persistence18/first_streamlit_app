90c7100b8db90a0ccd5d1e5d5f11eced9f60ce36
persistence18 <persistence18@gmail.com>
2022-09-08 14:10:54 +0000

    use different display methods

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
-streamlit.markdown('## Breakfast Menu')
-streamlit.markdown('Omega 3 & Bluberry Oatmeal')
-streamlit.markdown('Kale, Spinach & Rocket Smoothie')
-streamlit.markdown('Hard-Boiled Free-Range Egg')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Bluberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
