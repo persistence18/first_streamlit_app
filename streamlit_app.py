e54214c41bbc53ae20318e920f8d4d0d5a7ea6e8
persistence18 <persistence18@gmail.com>
2022-09-08 14:07:50 +0000

    update menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+streamlit.markdown('## Breakfast Menu')
+streamlit.markdown('Omega 3 & Bluberry Oatmeal')
+streamlit.markdown('Kale, Spinach & Rocket Smoothie')
+streamlit.markdown('Hard-Boiled Free-Range Egg')
